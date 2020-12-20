71e96a752287340fafab48a9b84774e1d5fe2ea7
Robert Hambrock <roberthambrock@gmail.com>
2020-12-20 04:18:55 +0100

    Lint with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate serde_derive;
 
+pub mod execution;
+pub mod nbpt;
+pub mod parse_regex;
 pub mod petrinet;
 pub mod protocol2;
-pub mod nbpt;
-pub mod execution;
-pub mod parse_regex;
